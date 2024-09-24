7117dd08e1252566060df04da23cd3de99395f73
Nikolai Lambrov <52069739+l8556@users.noreply.github.com>
2024-09-24 15:59:40 +0300

    Fix bug 70398 (#8)
    * Fix bug 70398
    
    * Fix bug 70398
    
    * Fix bug 70398
    
    * Fix bug 70398

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
 
-# Press the green button in the gutter to run the script.
+# Press the green button in the
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
